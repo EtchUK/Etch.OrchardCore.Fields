f3b9e1319bc0799eb0c283ec3ddc89fada784cc7
Peter Keating <peter@moov2.com>
2019-04-13 11:05:24 +0100

    Create Orchard Core module project

---
+++ Manifest.cs
@@ -0,0 +1,15 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "Useful Fields",
+    Author = "Moov2",
+    Website = "https://moov2.com",
+    Version = "0.0.1"
+)]
+
+[assembly: Feature(
+    Id = "Moov2.OrchardCore.Fields",
+    Name = "Useful Fields",
+    Description = "Provides a collection of useful fields.",
+    Category = "Content"
+)]
