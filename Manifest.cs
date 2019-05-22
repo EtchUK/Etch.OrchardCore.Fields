3be9c4c04276b8429f36f9506fd0c33350cb8a0e
Peter Keating <peter@moov2.com>
2019-05-22 11:10:26 +0100

    Bump to 0.3.0

--- Manifest.cs
+++ Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.Modules.Manifest;
     Name = "Useful Fields",
     Author = "Etch UK",
     Website = "https://etchuk.com",
-    Version = "0.2.0"
+    Version = "0.3.0"
 )]
 
 [assembly: Feature(
