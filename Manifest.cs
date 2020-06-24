94f02e5de23de23fdcde1d0e862a9e8760374f7a
Peter Keating <pete.keating@etchuk.com>
2020-06-25 00:13:48 +0100

    v0.9.1

--- Manifest.cs
+++ Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.Modules.Manifest;
     Name = "Useful Fields",
     Author = "Etch UK",
     Website = "https://etchuk.com",
-    Version = "0.9.0"
+    Version = "0.9.1"
 )]
 
 [assembly: Feature(
