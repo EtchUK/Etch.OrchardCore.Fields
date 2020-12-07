d3e2b9d4271050a236667744e10e36595634b77d
Peter Keating <pete.keating@etchuk.com>
2020-12-07 22:42:18 +0000

    Improve defining of available colours on `ColourField`
    Colours have been converted to a name/value collection where the name is
    now displayed underneath the colour on the field editor.

---
+++ Colour/Settings/ColourItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Etch.OrchardCore.Fields.Colour.Settings
+{
+    public class ColourItem
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
