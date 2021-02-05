e190f115b73428718033fd09b6bc891e07dd6b5c
Peter Keating <pete.keating@etchuk.com>
2021-02-05 22:51:50 +0000

    Reduce duplicate code by using partial for possible colours field

---
+++ Colour/ViewModels/ColoursFieldViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Etch.OrchardCore.Fields.Colour.ViewModels
+{
+    public class ColoursFieldViewModel
+    {
+        public string Colours { get; set; }
+
+        public string FieldId { get; set; }
+    }
+}
