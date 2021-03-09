e734c977d2485a054a2759460572c4e115f7f711
Peter Keating <pete.keating@etchuk.com>
2021-03-09 21:17:48 +0000

    Fix boolean settings on colour field not saving
    Issue was the default values being set on the property was preventing
    the properties from taking the values set by the user.
    
    Fixes #74.

--- Colour/Settings/ColourFieldSettings.cs
+++ Colour/Settings/ColourFieldSettings.cs
@@ -4,11 +4,11 @@ namespace Etch.OrchardCore.Fields.Colour.Settings
 {
     public class ColourFieldSettings
     {
-        public bool AllowCustom { get; set; } = true;
-        public bool AllowTransparent { get; set; } = true;
+        public bool AllowCustom { get; set; }
+        public bool AllowTransparent { get; set; }
         public ColourItem[] Colours { get; set; } = Array.Empty<ColourItem>();
         public string DefaultValue { get; set; }
         public string Hint { get; set; }
-        public bool UseGlobalColours { get; set; } = false;
+        public bool UseGlobalColours { get; set; }
     }
 }
