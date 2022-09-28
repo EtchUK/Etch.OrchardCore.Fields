eedc541d99368527bc5720eba5d9bde289a35735
Peter Keating <pete.keating@etchuk.com>
2022-09-28 08:31:51 +0100

    Various fixes around bootstrap 5 upgrade
    - Fix modals not functioning
    - Update CSS classes for margins, custom checkboxes and field layout
    - Fix dropdown menu button not functioning

--- Assets/ResponsiveMedia/js/components/responsiveMediaEditor/components/responsiveMediaItem/index.ts
+++ Assets/ResponsiveMedia/js/components/responsiveMediaEditor/components/responsiveMediaItem/index.ts
@@ -91,7 +91,7 @@ export default Vue.extend({
                 <button 
                     v-for="breakpoint in breakpoints" 
                     type="button" 
-                    class="btn btn-secondary btn-sm mb-1 mr-1" 
+                    class="btn btn-secondary btn-sm mb-1 me-1" 
                     v-on:click="selectedBreakpoint = breakpoint" 
                     v-bind:class="{ active: activeBreakpoint == breakpoint, 'btn-warning': hasNoSource(breakpoint) }">
                     {{ breakpoint }}
@@ -99,12 +99,12 @@ export default Vue.extend({
             </div>
 
             <div class="d-flex flex-row flex-wrap mb-2">
-                <a href="#" class="mr-3" v-on:click.prevent="update">Change image at {{ activeBreakpoint }}</a>
+                <a href="#" class="me-3" v-on:click.prevent="update">Change image at {{ activeBreakpoint }}</a>
             </div>
 
             <div class="btn-toolbar">
-                <div class="btn-group mr-2">
-                    <button type="button" title="Remove Options" class="btn btn-danger btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <div class="btn-group me-2">
+                    <button type="button" title="Remove Options" class="btn btn-danger btn-sm dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <svg class="svg-inline--fa fa-trash-alt fa-w-14" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="trash-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"></path></svg>
                     </button>
                     <div class="dropdown-menu">
